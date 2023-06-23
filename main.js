b1fc91fdd55aed0fadf9e608d5a9d12cd8ea20ed
MissBonnie1996 <109617470+MissBonnie1996@users.noreply.github.com>
2023-06-23 15:19:05 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var currentImage = 1;
+var totalImages= 12;
+
+var image=[
+    "images/AjolotesVengadoresMayJun23_b-1.png",
+    "images/AjolotesVengadoresMayJun23_b-2.png",
+    "images/AjolotesVengadoresMayJun23_b-3.png",
+    "images/AjolotesVengadoresMayJun23_b-4.png",
+    "images/AjolotesVengadoresMayJun23_b-5.png",
+    "images/AjolotesVengadoresMayJun23_b-6.png",
+    "images/AjolotesVengadoresMayJun23_b-7.png",
+    "images/AjolotesVengadoresMayJun23_b-8.png",
+    "images/AjolotesVengadoresMayJun23_b-9.png",
+    "images/AjolotesVengadoresMayJun23_b-10.png",
+    "images/AjolotesVengadoresMayJun23_b-11.png",
+    "images/AjolotesVengadoresMayJun23_b-12.png",
+];
+var currentImage = 1;
+var total = 12;
+
+
+function atras() {
+    if (currentImage > 1) {
+      currentImage--;
+    } else {
+      currentImage = total;
+    }
+    updateImage();
+  }
+  
+  function siguiente() {
+    if (currentImage < total) {
+      currentImage++;
+    } else {
+      currentImage = 1;
+    }
+    updateImage();
+  }
+  
+  function updateImage() {
+    var image = document.getElementById('album');
+    image.src = 'images/AjolotesVengadoresMayJun23_b-' + currentImage + '.png';
+  }
+
+
